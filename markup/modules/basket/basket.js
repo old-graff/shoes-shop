313cda46adf65cbe5eae49a0ae9d3afc6e59ad1a
lindal <lindal@lindal-Inspiron-3521>
2015-11-21 22:18:53 +0400

    test

--- markup/modules/basket/basket.js
+++ markup/modules/basket/basket.js
@@ -1,6 +1,10 @@
 app.controller('BasketController', function ($scope, BasketService) {
     $scope.products = BasketService.getProducts();
     $scope.totalPrice = function () {
-        return 32131;
+        var sum = 0;
+        for (var i = 0; i < $scope.products.length; i++) {
+            sum += $scope.products[i].price;
+        }
+        return sum;
     };
 });
