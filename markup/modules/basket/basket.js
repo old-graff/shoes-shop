01449f49fe17100708956507f958b6656d64cfe8
Lindal <lindal@mail.ru>
2015-11-25 02:14:13 +0400

    js

--- markup/modules/basket/basket.js
+++ markup/modules/basket/basket.js
@@ -1,10 +1,60 @@
-app.controller('BasketController', function ($scope, BasketService) {
-    $scope.products = BasketService.getProducts();
-    $scope.totalPrice = function () {
-        var sum = 0;
-        for (var i = 0; i < $scope.products.length; i++) {
-            sum += $scope.products[i].price;
-        }
-        return sum;
-    };
-});
+var Basket = {
+    block: false,
+    add: function (goodID, sizeID) {
+        $.ajax({
+            url: this.getAddUrl(),
+            data: { goodID: goodID, sizeID: sizeID },
+            dataType: 'json',
+            success: function (data) {
+                if (data) {
+                    this.render(data);
+                }
+            }.bind(this)
+        });
+        console.log('add in basket');
+    },
+    delete: function (id) {
+        console.log('delete from basket');
+        $.ajax({
+            url: this.getDeleteUrl(),
+            data: { id: id },
+            dataType: 'json',
+            success: function (data) {
+                this.getElement(id).remove();
+            }.bind(this)
+        });
+    },
+    updateCount: function (id, count) {
+        console.log('set count');
+        $.ajax({
+            url: this.getUpdateUrl(),
+            data: { id: id, count: count },
+            dataType: 'json',
+            success: function (data) {
+                this.render(data);
+            }.bind(this)
+        });
+        return false;
+    },
+    render: function (el) {
+        console.log('render');
+        $('.basket').html($(el).html());
+
+    },
+    check: function (goodID, sizeID) {
+        console.log('check');
+        return false;
+    },
+    getElement: function (id) {
+        return $('.product-card-micro[data-basket-id=' + id + ']');
+    },
+    getAddUrl: function () {
+        return $('.basket').attr('data-add-url');
+    },
+    getDeleteUrl: function () {
+        return $('.basket').attr('data-delete-url');
+    },
+    getUpdateUrl: function () {
+        return $('.basket').attr('data-update-url');
+    }
+};
