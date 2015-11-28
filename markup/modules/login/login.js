e90a8fbc15ef43310845c47e68bfa0628eebef17
Lindal <lindal@mail.ru>
2015-11-28 16:14:06 +0400

    js fix

--- markup/modules/login/login.js
+++ markup/modules/login/login.js
@@ -1,8 +1,8 @@
-$('.js-open-login-form').click(function () {
-    if ($('.login__form').is(':hidden')) {
-        $('.login__form').show();
-    } else {
-        $('.login__form').hide();
-    }
-    return false;
-});
+// $('.js-open-login-form').click(function () {
+//     if ($('.login__form').is(':hidden')) {
+//         $('.login__form').show();
+//     } else {
+//         $('.login__form').hide();
+//     }
+//     return false;
+// });
--- markup/modules/popup/popup.js
+++ markup/modules/popup/popup.js
@@ -1,13 +1,9 @@
 app.controller('AuthController', function ($scope, $rootScope) {
     $scope.state = 'reg';
-    $scope.open = $rootScope.showPopup;
     $scope.checkState = function (checkedState) {
         return $scope.state == checkedState;
     };
     $scope.setState = function (newState) {
         $scope.state = newState;
-    };
-    $scope.closePopup = function () {
-        $scope.open = false;
     };
 });
